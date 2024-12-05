ac1df66988f593b039fd14fa44f642afa2bc8940
karunakargoud <karunakargoud1116@gmail.com>
2024-12-05 23:33:14 +0530

    FirstCommit

---
+++ PartialClass/Product.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    partial class Product
+    {
+        partial void AddProduct();
+        partial void EditProduct();
+        partial void FindProduct();
+        partial void DeleteProduct();
+    }
+    //Developer1
+     partial class Product
+    {
+        partial void AddProduct()
+        {
+            Console.WriteLine("AddProduct by Developer1");
+        }
+        public void SumProduct()
+        {
+            AddProduct();
+        }
+    }
+    //Developer2
+    partial class Product
+    {
+        partial void EditProduct()
+        { 
+            Console.WriteLine("EditProduct by Developer2");
+      
+         }
+        public void UpdateProduct()
+        {
+            EditProduct();
+        }
+    }
+
+    //Developer3
+     partial class Product
+    {
+        partial void FindProduct()
+        {
+            Console.WriteLine("FindProduct by Developer3");
+        }
+        public void SearchProduct()
+        {
+            FindProduct();
+        }
+    }
+    //Developer4
+    partial class Product
+    {
+        partial void DeleteProduct()
+        {
+            Console.WriteLine("RemoveProduct by Developer4");
+        }
+        public void  RemoveProduct()
+        {
+            DeleteProduct();
+        }
+    }
+
+}
+
---
+++ PartialClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p=new Product();
+
+            p.SumProduct();
+            p.UpdateProduct();
+            p.SearchProduct();
+            p.RemoveProduct();
+
+        }
+    }
+}
